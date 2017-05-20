bcd2727fc62578800ffb9377c1c53e4946335b19
xhkr <hugo.kreipke@gmail.com>
2017-05-20 17:23:45 +0200

    test: Heroku environment test #3

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 var Twitter = require('twitter');
-var config = require('./config');
+//var config = require('./config');
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
