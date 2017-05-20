96507bb5dbf44fd82f60002de9e9f44bd7e58398
xhkr <hugo.kreipke@gmail.com>
2017-05-20 16:41:22 +0200

    test: production/development env

--- index.js
+++ index.js
@@ -8,7 +8,24 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'pug');
 
-var client = new Twitter(config);
+// views is directory for all template files
+
+var env = process.env.NODE_ENV || 'development';
+
+if (env === 'development') {
+
+    var client = new Twitter(config);
+
+} else {
+
+    var client = new Twitter({
+        consumer_key: process.env.consumer_key,
+        consumer_secret: process.env.consumer_secret,
+        access_token_key: process.env.access_token,
+        access_token_secret: process.env.access_token_secret
+    });
+
+}
 
 app.get('/', function(req, res) {
 
