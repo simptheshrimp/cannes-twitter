db7e28363e03c7f6a20ccd3634df526d9ee83f35
xhkr <hugo.kreipke@gmail.com>
2017-05-20 17:30:12 +0200

    test: Heroku environment test #4

--- index.js
+++ index.js
@@ -1,21 +1,25 @@
 var express = require('express');
 var app = express();
 var Twitter = require('twitter');
-//var config = require('./config');
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'pug');
 
+var env = process.env.NODE_ENV || 'development';
 
-var client = new Twitter({
-    consumer_key: process.env.consumer_key,
-    consumer_secret: process.env.consumer_secret,
-    access_token_key: process.env.access_token,
-    access_token_secret: process.env.access_token_secret
-});
-
+if (env === 'development') {
+    var config = require('./config');
+    var client = new Twitter(config);
+} else if (env === 'production'){
+    var client = new Twitter({
+        consumer_key: process.env.consumer_key,
+        consumer_secret: process.env.consumer_secret,
+        access_token_key: process.env.access_token,
+        access_token_secret: process.env.access_token_secret
+    });
+};
 
 app.get('/', function(req, res) {
 
