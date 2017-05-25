5d5b656275ee425bc3cc2d9ccaeb3655b7537ca3
xhkr <hugo.kreipke@gmail.com>
2017-05-25 19:08:19 +0200

    styles: Border and colors CSS

--- public/js/main.js
+++ public/js/main.js
@@ -12,12 +12,9 @@ socket.on('tweet', function (tweet) {
     var html = '<div class="tweet-live-icon">';
         html += '<img class="tweet-live-header__icon" src="' + tweet.user.profile_image_url + '">';
         html += '</div>';
+
         html += '<div class="tweet-live-content">';
         html += '<span class="tweet-live-header__user">' + tweet.user.name + '</span>';
-        // If tweet has image
-    if (tweet.entities.media && tweet.entities.media.length > 0) {
-        html += '<img class="tweet-content__img" src="' + tweet.entities.media[0].media_url + ':thumb">';
-    }
         html += '<p class="tweet-content__text">' + tweet.text + '</p>';
         html += '</div>';
 
